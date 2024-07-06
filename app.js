9c8045b6d556909d72100a4a7dc86883f6d3a2ee
jawadmehmood <jawadam5678@gmail.com>
2024-07-07 04:19:01 +0500

    JS Variable Names Completed

---
+++ app.js
@@ -0,0 +1,27 @@
+// Assignment 1
+
+var fullName , age , religion
+
+// Assignment 2
+
+// legal variable names
+var fullName
+var age
+var nationality
+var $num1
+var _num2
+
+// illegal variable names
+// var 1num  
+// var +age
+// var %percent
+// var @email
+// var <hello>
+
+// Assignment 3
+
+document.write('<h1>Rules for naming JS variables</h1><br />');
+document.write('Variable names can only contain ________ , ________ , ________ and ________ . For example: <b>$my_1stVariable</b><br />');
+document.write('Variables must begin with a ________ , ________ or ________ . For example: <b>$name , _name or name</b><br />');
+document.write('Variable names are case ________ <br />');
+document.write('Variable names should not be JS ________');
